9c8e889f92528931d0fb322f2b9a92b73f589f9f
lilac <cnjamesdeng@gmail.com>
2021-06-03 13:45:21 +0800

    Implement concurrent run from scratch

--- core/src/main/scala/Main.scala
+++ core/src/main/scala/Main.scala
@@ -1,6 +1,4 @@
-import cats.effect.implicits._
-import cats.effect.{ExitCode, IO, IOApp}
-import cats.implicits._
+import cats.effect.{ExitCode, FiberIO, IO, IOApp}
 
 import scala.concurrent.duration.DurationInt
 
@@ -8,14 +6,45 @@ object Main extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = for {
     start <- IO.realTime
-    results <- 1.to(1000)
+    workers = 1.to(1000)
       .toList
-      .parUnorderedTraverse(worker)
+      .map(worker)
+    fibers <- startAll(workers)
+    results <- awaitAll(fibers)
     end <- IO.realTime
   } yield {
     println(s"Time: ${end - start}")
     println(s"Results: $results")
     ExitCode.Success
+  }
+
+  def startAll[T](tasks: Seq[IO[T]]): IO[Seq[FiberIO[T]]] = {
+    tasks match {
+      case Seq() => IO.pure(Seq.empty)
+      case Seq(t) => t.start.map(Seq(_))
+      case a :: b :: other =>
+        for {
+          fa <- a.start
+          fb <- b.start
+          fo <- startAll(other)
+        } yield Seq(fa, fb) ++ fo
+    }
+  }
+
+  def awaitAll[T](fibers: Seq[FiberIO[T]]): IO[Seq[T]] = {
+    fibers match {
+      case Seq() => IO.pure(Seq.empty)
+      case Seq(f) =>
+        for {
+          outcome <- f.joinWithNever
+        } yield Seq(outcome)
+      case fa :: fb :: fs =>
+        for {
+          ra <- fa.joinWithNever
+          rb <- fb.joinWithNever
+          ro <- awaitAll(fs)
+        } yield Seq(ra, rb) ++ ro
+    }
   }
 
   def worker(id: Int): IO[Int] = for {
