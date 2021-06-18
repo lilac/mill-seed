1adaa146e566bd70cfe491554890ca46c9ea35e7
lilac <cnjamesdeng@gmail.com>
2021-06-18 14:30:56 +0800

    Refactor recursive functions to iterative mutations

--- core/src/main/scala/IOExtension.scala
+++ core/src/main/scala/IOExtension.scala
@@ -2,31 +2,24 @@ import cats.effect.{FiberIO, IO}
 
 object IOExtension {
   def awaitAll[T](fibers: List[FiberIO[T]]): IO[List[T]] = {
-    fibers match {
-      case Nil => IO.pure(Nil)
-      case f :: Nil =>
-        for {
-          outcome <- f.joinWithNever
-        } yield List(outcome)
-      case fa :: fb :: fs =>
-        for {
-          ra <- fa.joinWithNever
-          rb <- fb.joinWithNever
-          ro <- IOExtension.awaitAll(fs)
-        } yield ra :: rb :: ro
+    var io: IO[List[T]] = IO.pure(List.empty)
+    for (fiber <- fibers) {
+      io = for {
+        res <- io
+        ra <- fiber.joinWithNever
+      } yield ra :: res
     }
+    io.map(_.reverse)
   }
 
   def startAll[T](tasks: List[IO[T]]): IO[List[FiberIO[T]]] = {
-    tasks match {
-      case Nil => IO.pure(List.empty)
-      case t :: Nil => t.start.map(List(_))
-      case a :: b :: other =>
-        for {
-          fa <- a.start
-          fb <- b.start
-          fo <- IOExtension.startAll(other)
-        } yield fa :: fb :: fo
+    var io: IO[List[FiberIO[T]]] = IO.pure(List.empty)
+    for (task <- tasks) {
+      io = for {
+        fibers <- io
+        fb <- task.start
+      } yield fb :: fibers
     }
+    io.map(_.reverse)
   }
 }
